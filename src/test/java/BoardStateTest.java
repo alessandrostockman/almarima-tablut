721e78ef8ab0032d6ade1c78e31bad9cfe5461d2
Alessandro Stockman <alessandrostockman@gmail.com>
2020-11-24 17:23:38 +0100

    add testing for player implementation and min max

--- src/test/java/BoardStateTest.java
+++ src/test/java/BoardStateTest.java
@@ -1,4 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.almarima.tablut.application.domain.BoardState;
+import it.unibo.almarima.tablut.application.domain.Coord;
+import it.unibo.almarima.tablut.external.State;
+import it.unibo.almarima.tablut.external.StateTablut;
+import it.unibo.almarima.tablut.external.State.Pawn;
+
 
 public class BoardStateTest {
 
+    @Test
+    public void test() throws UnknownHostException, IOException {
+        State s = new StateTablut();
+        BoardState b = new BoardState(s);
+        assertEquals(b.getPawnAt(new Coord(4, 4)), Pawn.KING);
+        assertNotEquals(b.getPawnAt(new Coord(0, 0)), Pawn.KING);
+    }
 }
---
+++ src/test/java/TablutPlayerTest.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.almarima.tablut.application.domain.BoardState;
+import it.unibo.almarima.tablut.application.domain.Move;
+import it.unibo.almarima.tablut.application.player.ImplPlayer;
+import it.unibo.almarima.tablut.application.player.TablutPlayer;
+import it.unibo.almarima.tablut.external.StateTablut;
+import it.unibo.almarima.tablut.external.State.Turn;
+import it.unibo.almarima.tablut.external.State;
+
+
+public class TablutPlayerTest {
+
+    @Test
+    public void testCreation() {
+        TablutPlayer whitePlayer = new ImplPlayer(60, Turn.WHITE);
+        TablutPlayer blackPlayer = new ImplPlayer(60, Turn.BLACK);
+        assertEquals(whitePlayer.getPlayerId(), BoardState.WHITE);
+        assertEquals(blackPlayer.getPlayerId(), BoardState.BLACK);
+    }
+
+    @Test
+    public void testComputeMove() {
+        State s = new StateTablut();
+        BoardState bs = new BoardState(s);
+        TablutPlayer whitePlayer = new ImplPlayer(60, Turn.WHITE);
+        whitePlayer.setBoardState(s);
+        
+        Move chosen = whitePlayer.computeMove();
+        boolean found = false;
+        for (Move m : bs.getAllLegalMoves()) {
+            if (m.equals(chosen)) {
+                found = true;
+            }
+        }
+        assertTrue(found);
+
+
+    }
+
+}
